6c7b060e4f2fa485d3041901d03c47bff2165459
Fede <i@2fd.me>
2019-06-10 12:33:50 -0300

    fix theme configuration

--- playground/theme.ts
+++ playground/theme.ts
@@ -1,6 +1,6 @@
 // tslint:disable:no-console
 import * as React from "react";
-import { defaultTheme } from "evergreen-ui/commonjs/theme/src/default-theme/index.js";
+import defaultTheme from "evergreen-ui/commonjs/theme/src/default-theme/index.js";
 const Theme = React.createContext(defaultTheme);
 const { Provider: ThemeProvider, Consumer: ThemeConsumer } = Theme;
 export default Theme;
