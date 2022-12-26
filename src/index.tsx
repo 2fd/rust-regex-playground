28fc954969bb41460f333c88603e6c1ee839ba81
frami <i@2fd.me>
2022-12-25 22:37:16 -0300

    chore: remove comments

--- src/index.tsx
+++ src/index.tsx
@@ -4,19 +4,7 @@ import './css/normalize.css'
 import './css/skeleton.css'
 
 import App from './components/Layout/App'
-
-// import App from './App'
-// import { ThemeProvider, defaultTheme } from './theme'
-// import { createStateFromHash, IState, updateHashFromState } from './common'
-// import rregex, { RRegExp, get_metadata } from './rregex'
-// import Store, { mergeHandlers, mergeStates } from './store'
-// import * as Evergreen from "evergreen-ui";
-
 const root = document.getElementById('root')
-// const store = new Store<IState>(
-//   mergeStates(createStateFromHash()),
-//   mergeHandlers(updateHashFromState, render)
-// )
 
 ReactDOM.render(<App />, root, () =>
   console.log(
@@ -34,38 +22,3 @@ ReactDOM.render(<App />, root, () =>
     'font-family:monospace;color:#666;'
   )
 )
-
-// async function feature(name: string, evaluate: Promise<any>) {
-//   return evaluate
-//     .then(value => store.dispatch({ [name]: value }))
-//     .catch((err: Error) => {
-//       console.error(err)
-//       store.dispatch({ [name]: false, [name + 'Error']: err })
-//     })
-// }
-
-// render()
-
-// Object.assign(window, { store, RRegExp })
-// window.addEventListener('hashchange', () =>
-//   store.dispatch(createStateFromHash())
-// )
-
-// feature(
-//   'features.completed',
-//   Promise.all([
-//     feature(
-//       'features.rregex',
-//       rregex.then(() => {
-//         const metadata = get_metadata()
-//         feature('versions.regex', Promise.resolve(metadata.regex))
-//         feature(
-//           'versions.regex_syntax',
-//           Promise.resolve(metadata['regex-syntax'])
-//         )
-//         return true
-//       })
-//     ),
-//     feature('features.share', Promise.resolve(!!(navigator as any).share)),
-//   ])
-// )
